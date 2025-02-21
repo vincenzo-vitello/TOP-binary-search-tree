f8e3e8c10b060f1b792329fb41ec52691e9bc669
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2025-02-21 12:35:58 +0100

    added methods to delete items, get tree height and depth, find items and check if the tree is balanced

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ class BinarySearchTree {
         this.root = null
     }
 
-    insert(item) {
+    insert(data) {
         if (!this.root) { 
-            this.root = new Node(item);
+            this.root = new Node(data);
             return;
         }
 
         function searchTree(node) {
-            if (item < node.data) {
+            if (data < node.data) {
                 if (!node.left) {
-                    node.left = new Node(item);
+                    node.left = new Node(data);
                     return;
                 } else {
                     searchTree(node.left);
                 }
-            } else if (item > node.data) {
+            } else if (data > node.data) {
                 if (!node.right) {
-                    node.right = new Node(item);
+                    node.right = new Node(data);
                     return;
                 } else {
                     searchTree(node.right);
@@ -40,23 +40,78 @@ class BinarySearchTree {
     }
 
     delete(data) {
+        function deleteNode(node, data) {
+            if(!node) return null
+            if(data === node.data) {
+                // check if node has no children
+                if(!node.left && !node.right) return null;
 
+                // check if node has only right children
+                if(!node.left) return node.right;
+
+                // check if node has only left children
+                if(!node.right) return node.left;
+
+                let temp = node.right;
+                while(temp.left) {
+                    temp = temp.left
+                }
+                node.data = temp.data;
+                node.right = deleteNode(node.right, temp.data);
+                return node;
+            } else if(data < node.data) {
+                node.left = deleteNode(node.left, data);
+                return node;
+            } else {
+                node.right = deleteNode(node.right, data);
+                return node;
+            }
+        }
+        this.root = deleteNode(this.root, data)
     }
 
     find(data) {
-
+        let cur = this.root;
+        while(cur) {
+            if(data === cur.data) return cur;
+            if(data < cur.data) cur = cur.left;
+            if(data > cur.data) cur = cur.right;
+        }
     }
 
-    height() {
+    height(node) {
+        if(!node) return -1;
+        
+        let left = this.height(node.left);
+        let right = this.height(node.right);
 
+        return 1 + Math.max(left, right)
     }
 
-    depth() {
+    depth(node) {
+        if(!node || !this.root) return -1;
 
+        let cur = this.root;
+        let counter = 0;
+
+        while(cur) {
+            if(node.data === cur.data) return counter;
+            if(node.data < cur.data) cur = cur.left;
+            if(node.data > cur.data) cur = cur.right;
+            counter++
+        }
+        return -1 
     }
 
-    isBalanced() {
+    isBalanced(node = this.root) {
+        if (!node) return true; 
+    
+        let leftHeight = this.height(node.left);
+        let rightHeight = this.height(node.right);
+    
+        if (Math.abs(leftHeight - rightHeight) > 1) return false; 
 
+        return this.isBalanced(node.left) && this.isBalanced(node.right);
     }
 
     rebalance() {
@@ -105,3 +160,16 @@ const myTree = new BinarySearchTree
 
 buildTree(data)
 prettyPrint(myTree.root)
+myTree.delete(1)
+prettyPrint(myTree.root)
+myTree.insert(1)
+prettyPrint(myTree.root)
+console.log(myTree.find(1))
+console.log("heigth: ", myTree.height(myTree.root))
+console.log("heigth: ", myTree.height(myTree.root.left.left.left));
+console.log("heigth: ", myTree.height(myTree.root.right.right))
+console.log("depth: ", myTree.depth(myTree.root.right))
+console.log("depth: ", myTree.depth(myTree.root.right.left))
+prettyPrint(myTree.root)
+console.log("is the tree balanced? ", myTree.isBalanced())
+
